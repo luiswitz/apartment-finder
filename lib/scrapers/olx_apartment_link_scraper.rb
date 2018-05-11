b3a2ba7917a54fcfd8311a6926ee90ac3aea5314
Luis Henrique Witz <luiswitz@gmail.com>
2018-05-11 22:51:07 +0100

    Olx Apartment Scraper

---
+++ lib/scrapers/olx_apartment_link_scraper.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+
+module Scrapers
+  class OlxApartmentLinkScraper
+    URL = 'https://www.olx.pt/imoveis/apartamento-casa-a-venda/apartamentos-arrenda/?search%5Bfilter_float_price%3Afrom%5D=300&search%5Bfilter_float_price%3Ato%5D=700&search%5Bfilter_enum_tipologia%5D%5B0%5D=t1&search%5Bfilter_enum_tipologia%5D%5B1%5D=t2&search%5Bdescription%5D=1'
+
+    def initialize(html = nil)
+      @html = html
+    end
+
+    def find_links
+      page.search('.detailsLink').map do |link|
+        link['href']
+      end
+    end
+
+    attr_reader :html
+
+    private
+
+    def page
+      return Nokogiri::HTML(html) if html
+
+      Nokogiri::HTML(open(URL))
+    end
+  end
+end
+
