1488964543878caa57503654a7fbc1e2044d3321
Luis Henrique Witz <luiswitz@gmail.com>
2018-05-17 08:06:07 +0100

    Refactor on apartment scrapers

--- lib/scrapers/custo_justo_apartment_link_scraper.rb
+++ lib/scrapers/custo_justo_apartment_link_scraper.rb
@@ -1,29 +1,16 @@
 # frozen_string_literal: true
 
-require 'open-uri'
-
 module Scrapers
-  class CustoJustoApartmentLinkScraper
-    URL = 'https://www.custojusto.pt/porto/apartamentos-arrendar?ps=3&pe=6&ros=4&roe=5'
-
-    def initialize(html = nil)
-      @html = html
-    end
-
-    def find_links
-      page.search('a[data-name="url"]').map do |link|
-        link['href']
-      end
-    end
-
-    attr_reader :html
+  class CustoJustoApartmentLinkScraper < ApartmentLinkScraperTemplate
 
     private
 
-    def page
-      return Nokogiri::HTML(html) if html
+    def anchor_link
+      'a[data-name="url"]'
+    end
 
-      Nokogiri::HTML(open(URL))
+    def url
+      'https://www.custojusto.pt/porto/apartamentos-arrendar?ps=3&pe=6&ros=4&roe=5'
     end
   end
 end
--- lib/scrapers/olx_apartment_link_scraper.rb
+++ lib/scrapers/olx_apartment_link_scraper.rb
@@ -1,29 +1,16 @@
 # frozen_string_literal: true
 
-require 'open-uri'
-
 module Scrapers
-  class OlxApartmentLinkScraper
-    URL = 'https://www.olx.pt/imoveis/apartamento-casa-a-venda/apartamentos-arrenda/porto/?search%5Bfilter_float_price%3Afrom%5D=300&search%5Bfilter_float_price%3Ato%5D=700&search%5Bfilter_enum_tipologia%5D%5B0%5D=t2&search%5Bdescription%5D=1'
-
-    def initialize(html = nil)
-      @html = html
-    end
-
-    def find_links
-      page.search('.detailsLink').map do |link|
-        link['href']
-      end
-    end
-
-    attr_reader :html
+  class OlxApartmentLinkScraper < ApartmentLinkScraperTemplate
 
     private
 
-    def page
-      return Nokogiri::HTML(html) if html
+    def anchor_link
+      '.detailsLink'
+    end
 
-      Nokogiri::HTML(open(URL))
+    def url
+      'https://www.olx.pt/imoveis/apartamento-casa-a-venda/apartamentos-arrenda/porto/?search%5Bfilter_float_price%3Afrom%5D=300&search%5Bfilter_float_price%3Ato%5D=700&search%5Bfilter_enum_tipologia%5D%5B0%5D=t2&search%5Bdescription%5D=1'
     end
   end
 end
