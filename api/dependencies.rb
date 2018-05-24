fd70485f5eddd53ebf9ef7fa73e101001629a1b5
Luis Henrique Witz <luiswitz@gmail.com>
2018-05-24 08:31:36 +0100

    Remove useless comments

--- api/dependencies.rb
+++ api/dependencies.rb
@@ -3,8 +3,6 @@ require 'nurse'
 class Dependencies < Nurse::DependencyContainer
   def initialize
     super
-    # p $".grep(/apartment/)
-    # p defined?(ApartmentScraperServiceFactory)
     add_factory(ApartmentScraperServiceFactory.new)
   end
 end
