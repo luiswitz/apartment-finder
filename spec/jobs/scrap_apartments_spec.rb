f1783f7c117441f383dc429034fb6a737162a1e6
Luis Henrique Witz <luiswitz@gmail.com>
2018-06-04 23:54:39 +0100

    wip

--- spec/jobs/scrap_apartments_spec.rb
+++ spec/jobs/scrap_apartments_spec.rb
@@ -3,21 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe ScrapApartments, type: :job do
-  include ActiveJob::TestHelper
+  subject { described_class.new }
 
-  subject { described_class }
+  describe '#perform' do
+    it 'receives the scraper service' do
+      expect(Dependencies).to receive(:get)
+        .with('apartment_scraper_service')
 
-  let(:job) { subject.perform_later }
-
-  # TODO: Fix those examples
-  xdescribe '#perform' do
-    it 'queues the job' do
-      expect(job).to have_enqueued_sidekiq_job('ScrapApartments')
-    end
-
-    it 'executes perform' do
-      expect(subject).to receive(:perform)
-      perform_enqueued_jobs { job }
     end
   end
 end
