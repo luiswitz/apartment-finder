7f1526546c96674c982ea5d89b8bcd45c326e27a
Luis Henrique Witz <luiswitz@gmail.com>
2018-06-21 00:06:21 +0100

    get telegram bot through telegram bot adapter factory

--- spec/jobs/scrap_apartments_spec.rb
+++ spec/jobs/scrap_apartments_spec.rb
@@ -11,13 +11,24 @@ RSpec.describe ScrapApartments, type: :job do
     instance_double(Services::ApartmentScraperService)
   end
 
+  let(:telegram_bot) do
+    instance_double(Adapters::TelegramBotAdapter)
+  end
+
   before do
     allow_any_instance_of(Dependencies).to receive(:get)
       .with('apartment_scraper_service')
       .and_return(apartment_scraper_service)
 
+    allow_any_instance_of(Dependencies).to receive(:get)
+      .with('api.adapters.telegram_bot_adapter')
+      .and_return(telegram_bot)
+
     allow(apartment_scraper_service).to receive(:find_links)
       .and_return(['link'])
+
+    allow(telegram_bot).to receive(:send_message)
+      .with('link')
   end
 
   describe '#perform' do
@@ -25,6 +36,12 @@ RSpec.describe ScrapApartments, type: :job do
       subject.perform
 
       expect(apartment_scraper_service).to have_received(:find_links)
+    end
+
+    it 'calls send_message method' do
+      subject.perform
+
+      expect(telegram_bot).to have_received(:send_message)
     end
   end
 end
